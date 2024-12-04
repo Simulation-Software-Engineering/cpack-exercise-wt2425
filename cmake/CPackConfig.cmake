cb77f5e0eb4b4068b316ee93aa5fd0234aff7655
Marcel W <st163937@stud.uni-stuttgart.de>
2024-12-04 17:27:56 +0100

    ADD: Fix the linter errors execept one

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -1,22 +1,38 @@
 # CPack configuration
-set(CPACK_PACKAGE_NAME "cpack-exercise")
+set(CPACK_PACKAGE_NAME "cpackexample")
 set(CPACK_PACKAGE_VERSION "0.1.0")
 set(CPACK_PACKAGE_VENDOR "Marcel Wolkober")
-set(CPACK_PACKAGE_CONTACT "st163937@stud.uni-stuttgart.de")
+#Linter: fix Maintainer
+set(CPACK_PACKAGE_CONTACT "Marcel Wolkober <st163937@stud.uni-stuttgart.de>")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A simple example project using CPack for the SSE Course")
+#Linter: fix description
+set(CPACK_PACKAGE_DESCRIPTION "This is a simple example project of the SSE Course of University Stuttgart to get used to packaging with CPack.")
 set(CPACK_PACKAGE_MAINTAINERS "Marcel Wolkober ${CPACK_PACKAGE_CONTACT}")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/MarcelWolkober/cpack-exercise-wt2425")
 set(CPACK_GENERATOR "TGZ;DEB")
 
 # Debian packaging section
-set(CPACK_DEBIAN_PACKAGE_NAME "cpack-exercise")
+set(CPACK_DEBIAN_PACKAGE_NAME "cpackexample")
 set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_CONTACT}")
-set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "${CPACK_PACKAGE_DESCRIPTION}")
 set(CPACK_DEBIAN_PACKAGE_SECTION "devel")
 set(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libyaml-cpp0.6, libboost-filesystem1.71.0, libdeal.ii-dev")
 set(CPACK_DEBIAN_FILE_NAME "DEB-DEFAULT")
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES)
+
+#add libyaml dependency
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libyaml-cpp0.8")
+
+#linter: strip files 
+set(CPACK_STRIP_FILES TRUE)
+
+# Install the COPYRIGHT file
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/cmake/files/copyright
+  DESTINATION ${CMAKE_INSTALL_DOCDIR})
+
+# Install the CHANGELOG file
+install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/cmake/files/changelog.gz
+  DESTINATION ${CMAKE_INSTALL_DOCDIR})
 
 include(CPack)
