02715de77ff111c25d588f8b0a97a551321f3a94
365Bit <20995752+365Bit@users.noreply.github.com>
2024-12-05 17:13:17 +0100

    Edits CMakeLists and adds CPackConfig

--- Dockerfile
+++ Dockerfile
@@ -13,13 +13,13 @@ RUN apt-get -qq update && \
         tree \
         lintian \
         unzip
-        
-# Get, unpack, build, and install yaml-cpp        
+
+# Get, unpack, build, and install yaml-cpp
 RUN mkdir software && cd software && \
     wget https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-0.6.3.zip && unzip yaml-cpp-0.6.3.zip && \
     cd yaml-cpp-yaml-cpp-0.6.3 && mkdir build && cd build && \
-    cmake -DYAML_BUILD_SHARED_LIBS=ON .. && make -j4 && make install    
-    
+    cmake -DYAML_BUILD_SHARED_LIBS=ON .. && make -j4 && make install
+
 # This is some strange Docker feature. Normally, you don't need to add /usr/local to these
 ENV LIBRARY_PATH $LIBRARY_PATH:/usr/local/lib/
 ENV LD_LIBRARY_PATH $LD_LIBRARY_PATH:/usr/local/lib/
---
+++ cmake/CPackConfig.cmake
@@ -0,0 +1,19 @@
+set(CPACK_PACKAGE_NAME "${PROJECT_NAME}")
+
+set(CPACK_PACKAGE_VENDOR "SSE Student")
+set(CPACK_PACKAGE_MAINTAINERS "tischlre")
+set(CPACK_PACKAGE_CONTACT "st149535@stud.uni-stuttgart.de")
+
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/365Bit/cpack-exercise-wt2425")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "SSE CPack example project")
+set(CPACK_PACKAGE_DESCRIPTION "The SSE CPack example Project demonstrates how to use CMake and CPack to create a distributable package.")
+
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+
+set(CPACK_GENERATOR "TGZ;DEB")
+
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES)
+
+include(CPack)
